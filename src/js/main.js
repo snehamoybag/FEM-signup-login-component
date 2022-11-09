37a88a3d47a26e2d051f50ff1f6adf7e3aee11ee
snehamoybag <snehamoybag2@gmail.com>
2022-11-10 01:19:31 +0530

    add javascript functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,61 @@
-'use strict'
+"use strict"
 // CSS STYLESHEET
-import '../scss/style.scss';
+import "../scss/style.scss";
+
+// JAVASCRIPT
+// Form elements
+const signupFormEl = document.querySelector("#signup-form");
+const inptFirstNameEl = document.querySelector("#first-name");
+const inptLastNameEl = document.querySelector("#last-name");
+const inptEmailEl = document.querySelector("#email");
+const inptPasswordEl = document.querySelector("#password");
+const btnSubmitEl = document.querySelector("#btn-submit");
+
+// Regular Expressions
+const regex = {
+  name: /^[a-zA-Z]{2,}$/,
+  email: /(^\S{1,64})@(\S*[a-zA-Z0-9]{1,255}$)/,
+  password: /^\S{8,32}$/,
+  whitespace: /\s/g
+};
+
+// Blueprint for the input validation
+function inputValidator(element, elName, elRegex, minLength, maxLength) {
+  const errMsg = element.nextElementSibling;
+  // validation
+  if (element.value === "" || element.value === null) {
+    errMsg.textContent = `${elName} cannot be empty`;
+  }
+  else if (regex.whitespace.test(element.value)) {
+    errMsg.textContent = `${elName} cannot contain any white space`;
+  }
+  else if (element.value.length < minLength) {
+    errMsg.textContent = `${elName} has to be atleast ${minLength} characters long`;
+  }
+  else if (element.value.length > maxLength) {
+    errMsg.textContent = `${elName} cannot be longer than ${maxLength} characters`
+  }
+  else if (!elRegex.test(element.value)) {
+    errMsg.textContent = `Looks like this is not a valid ${elName}`;
+  }
+  else {
+    errMsg.textContent = "";
+  }
+};
+
+// Doing input validation while user types
+inptFirstNameEl.addEventListener("input", () => { inputValidator(inptFirstNameEl, "First Name", regex.name, 2, 60) });
+inptLastNameEl.addEventListener("input", () => { inputValidator(inptLastNameEl, "Last Name", regex.name, 2, 60) });
+inptEmailEl.addEventListener("input", () => { inputValidator(inptEmailEl, "Email", regex.email, 3, 320) });
+inptPasswordEl.addEventListener("input", () => { inputValidator(inptPasswordEl, "Password", regex.password, 8, 48) });
+
+// Prevent form submiting when a input is invalid 
+signupFormEl.addEventListener("submit", e => {
+  if (
+    regex.name.test(inptFirstNameEl.value) &&
+    regex.name.test(inptLastNameEl.value) &&
+    regex.email.test(inptEmailEl.value) &&
+    regex.password.test(inptPasswordEl.value)
+  ) { alert(`Thankyou for Signing-Up ${inptFirstNameEl.value} ${inptLastNameEl.value}`); }
+  else { e.preventDefault(); }
+});
