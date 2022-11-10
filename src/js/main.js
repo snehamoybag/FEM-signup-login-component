9f9eb10666807493ec393b5255726749373b6054
snehamoybag <snehamoybag2@gmail.com>
2022-11-10 11:07:17 +0530

    fix js scalability

--- src/js/main.js
+++ src/js/main.js
@@ -20,34 +20,58 @@ const regex = {
 };
 
 // Blueprint for the input validation
-function inputValidator(element, elName, elRegex, minLength, maxLength) {
-  const errMsg = element.nextElementSibling;
-  // validation
-  if (element.value === "" || element.value === null) {
-    errMsg.textContent = `${elName} cannot be empty`;
+function InputValidator(element, elName, elRegex, minLength, maxLength) {
+  return {
+    element,
+    name,
+    elRegex,
+    minLength,
+    maxLength,
+    
+    validate() {
+      const errMsg = element.nextElementSibling;
+      // validation
+      if (element.value === "" || element.value === null) {
+        errMsg.textContent = `${elName} cannot be empty`;
+      }
+      else if (regex.whitespace.test(element.value)) {
+        errMsg.textContent = `${elName} cannot contain any white space`;
+      }
+      else if (element.value.length < minLength) {
+        errMsg.textContent = `${elName} has to be atleast ${minLength} characters long`;
+      }
+      else if (element.value.length > maxLength) {
+        errMsg.textContent = `${elName} cannot be longer than ${maxLength} characters`
+      }
+      else if (!elRegex.test(element.value)) {
+        errMsg.textContent = `Looks like this is not a valid ${elName}`;
+      }
+      else {
+        errMsg.textContent = "";
+      }
+    }
   }
-  else if (regex.whitespace.test(element.value)) {
-    errMsg.textContent = `${elName} cannot contain any white space`;
-  }
-  else if (element.value.length < minLength) {
-    errMsg.textContent = `${elName} has to be atleast ${minLength} characters long`;
-  }
-  else if (element.value.length > maxLength) {
-    errMsg.textContent = `${elName} cannot be longer than ${maxLength} characters`
-  }
-  else if (!elRegex.test(element.value)) {
-    errMsg.textContent = `Looks like this is not a valid ${elName}`;
-  }
-  else {
-    errMsg.textContent = "";
-  }
-};
+}
 
-// Doing input validation while user types
-inptFirstNameEl.addEventListener("input", () => { inputValidator(inptFirstNameEl, "First Name", regex.name, 2, 60) });
-inptLastNameEl.addEventListener("input", () => { inputValidator(inptLastNameEl, "Last Name", regex.name, 2, 60) });
-inptEmailEl.addEventListener("input", () => { inputValidator(inptEmailEl, "Email", regex.email, 3, 320) });
-inptPasswordEl.addEventListener("input", () => { inputValidator(inptPasswordEl, "Password", regex.password, 8, 48) });
+// Creating input objects
+const firstNameObj = InputValidator(inptFirstNameEl, "First Name", regex.name, 2, 60);
+const lastNameObj = InputValidator(inptLastNameEl, "Last Name", regex.name, 2, 60);
+const emailObj = InputValidator(inptEmailEl, "Email", regex.email, 3, 320);
+const passwordObj = InputValidator(inptPasswordEl, "Password", regex.password, 8, 48);
+
+// Doing live validation
+inptFirstNameEl.addEventListener("input", () => {
+  firstNameObj.validate();
+});
+inptLastNameEl.addEventListener("input", () => {
+  lastNameObj.validate();
+});
+inptEmailEl.addEventListener("input", () => {
+  emailObj.validate();
+});
+inptPasswordEl.addEventListener("input", () => {
+  passwordObj.validate();
+});
 
 // Prevent form submiting when a input is invalid 
 signupFormEl.addEventListener("submit", e => {
@@ -56,6 +80,6 @@ signupFormEl.addEventListener("submit", e => {
     regex.name.test(inptLastNameEl.value) &&
     regex.email.test(inptEmailEl.value) &&
     regex.password.test(inptPasswordEl.value)
-  ) { alert(`Thankyou for Signing-Up ${inptFirstNameEl.value} ${inptLastNameEl.value}`); }
+  ) { alert(`Thankyou for Signing Up ${inptFirstNameEl.value} ${inptLastNameEl.value}!`); }
   else { e.preventDefault(); }
 });
